8e431bed3a0cdc9a9a1d1ca766ee1cc9b70f626c
Breno Machado <burracha.6464@gmail.com>
2024-08-10 22:17:12 -0300

    Finalização do BubbleSort

--- BubbleSort/implementacao_01.cpp
+++ BubbleSort/implementacao_01.cpp
@@ -1,24 +1,38 @@
+//Implementação que saiu da minha cabeça:
+
 #include <iostream>
 using namespace std;
 
-void bubblesort(int ver[7])
+void bubblesort(int vet[7])
 {   
-    for ( int i = 0 ; i<7 ; i++)
+    int aux = 0; // variávcel que vai servir de auxíliar para a realização das trocas
+
+    for(int j = 0 ; j<7 ; j++) //for que vai garantir que a implementação vai ser executada até o vetor inteiro ser ordenado 
     {
-        
+        for ( int i = 0 ; i<7 ; i++) //for que vai percorrer o vetor
+        {
+            if (vet[i] > vet [i+1]) // caso o elemento referência seja maior que o adjacente
+            {
+                //realização da troca
+                aux = vet[i];
+                vet[i] = vet[i+1];
+                vet[i+1] = aux;
+            
+            }
+         }
     }
 
 }
 
 int main ()
 {   
-    int vet [7] = {2,7,3,6,2,1,4};
+    int vet [7] = {2,7,3,6,2,1,4}; // declaração do vetor teste
 
-    bubblesort(vet);
+    bubblesort(vet); // chamada da função
 
-    for (int i = 0 ; i<7 ; i++)
+    for (int i = 0 ; i<7 ; i++) //for que vai percorrer o vetor para imprimir seus elementos
     {
-        cout << vet[i] << endl;
+        cout << vet[i] << " ";  // impressão de elementos
     }
 
     return 0;
@@ -34,7 +48,7 @@ int main ()
 
 Em cada comparação, o algoritmo verifica se o adjacente é menor que o elemento referencial. Caso seja menor, eles trocam de lugar.
 
-Ao longo de toda a execução o vetor será percorrido o suficiente para ordenar todos os elemento, e será perceptível que os maiores elementos irão “subir” no vetor tal qual uma bolha em uma superfície líquida. Por isso o nome Bubble sort;
+Ao longo de toda a execução o vetor será percorrido o suficiente para ordenar todos os elementos, e será perceptível que os maiores elementos irão “subir” no vetor tal qual uma bolha em uma superfície líquida. Por isso o nome Bubble sort;
 
 custo assintótico = O(n^2)
 
